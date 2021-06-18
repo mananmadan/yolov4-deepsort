221abbb810be23966743120ebfc41a242099bdeb
mananmadan <madan.manan1@gmail.com>
2021-06-18 17:36:25 +0530

    don't need output file .. save time on that

--- object_tracker-logo.py
+++ object_tracker-logo.py
@@ -233,6 +233,7 @@ def main(_argv):
             bbox = track.to_tlbr()
             class_name = track.get_class()
             # draw bbox on screen
+            '''
             color = colors[int(track.track_id) % len(colors)]
             color = [i * 255 for i in color]
             cv2.rectangle(frame, (int(bbox[0]), int(
@@ -241,6 +242,7 @@ def main(_argv):
                 len(class_name)+len(str(track.track_id)))*17, int(bbox[1])), color, -1)
             cv2.putText(frame, class_name + "-" + str(track.track_id),
                         (int(bbox[0]), int(bbox[1]-10)), 0, 0.75, (255, 255, 255), 2)
+            '''
 
             # if enable info flag then print details and get store the dict
             if FLAGS.info:
@@ -289,6 +291,7 @@ def main(_argv):
         # calculate frames per second of running detections
         fps = 1.0 / (time.time() - start_time)
         print("FPS: %.2f" % fps)
+        '''
         result = np.asarray(frame)
         result = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
@@ -298,6 +301,7 @@ def main(_argv):
         # if output flag is set, save video file
         if FLAGS.output:
             out.write(result)
+        '''
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     logging.info("stopping the timer")
